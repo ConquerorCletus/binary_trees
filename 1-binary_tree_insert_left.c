a50e12bf21cd488ba97e66f7952d9dd97cbda213
ConquerorCletus <smartchibuike2019@gmail.com>
2023-08-02 20:06:07 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,8 +10,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	binary_tree_t *left_t;
 
-	left_t = (binary_tree_t *)malloc(sizeof(binary_tree_t));
-
 	if (parent == NULL)
 		return (NULL);
 
